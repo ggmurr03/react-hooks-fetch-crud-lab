b7022ce91a5eb2e1dfcf7edb0080edc33424eb01
Gregory Murray <gr.j.murray@gmail.com>
2024-11-28 05:43:26 -0800

    all tests passing

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, questions, setQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,7 +8,17 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
-
+  function handleClick() {
+    fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" })
+      .then((resp) => {
+        if (resp.ok) {setQuestions(questions.filter((q) => q.id !== question.id));
+        } else {console.error("Failed to delete the question");
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting the question:", error);
+      });
+  }
   return (
     <li>
       <h4>Question {id}</h4>
@@ -17,7 +27,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -8,7 +8,7 @@ function QuestionList({questions, setQuestions}) {
     <section>
       <h1>Quiz Questions</h1>
       <ul>{
-        questions.map((question, index)=> <QuestionItem key={index} question={question}/>)
+        questions.map((question, index)=> <QuestionItem questions={questions} setQuestions={setQuestions} key={index} question={question}/>)
     }</ul>
     </section>
   );
